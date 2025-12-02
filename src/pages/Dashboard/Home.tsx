42d37a8ac1d27a7e700054e4e305b5eee8c4ef26
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2025-12-02 15:05:22 +0530

    Change Home page

--- src/pages/Dashboard/Home.tsx
+++ src/pages/Dashboard/Home.tsx
@@ -1,9 +1,3 @@
-import EcommerceMetrics from "../../components/ecommerce/EcommerceMetrics";
-import MonthlySalesChart from "../../components/ecommerce/MonthlySalesChart";
-import StatisticsChart from "../../components/ecommerce/StatisticsChart";
-import MonthlyTarget from "../../components/ecommerce/MonthlyTarget";
-import RecentOrders from "../../components/ecommerce/RecentOrders";
-import DemographicCard from "../../components/ecommerce/DemographicCard";
 import PageMeta from "../../components/common/PageMeta";
 
 export default function Home() {
@@ -13,27 +7,16 @@ export default function Home() {
         title="React.js Ecommerce Dashboard | TailAdmin - React.js Admin Dashboard Template"
         description="This is React.js Ecommerce Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
       />
-      <div className="grid grid-cols-12 gap-4 md:gap-6">
-        <div className="col-span-12 space-y-6 xl:col-span-7">
-          <EcommerceMetrics />
-
-          <MonthlySalesChart />
-        </div>
-
-        <div className="col-span-12 xl:col-span-5">
-          <MonthlyTarget />
-        </div>
-
-        <div className="col-span-12">
-          <StatisticsChart />
-        </div>
-
-        <div className="col-span-12 xl:col-span-5">
-          <DemographicCard />
-        </div>
-
-        <div className="col-span-12 xl:col-span-7">
-          <RecentOrders />
+      
+      <div className="min-h-screen flex items-center justify-center bg-gray-50 p-4">
+        <div className="text-center">
+          <h2 className="text-6xl md:text-7xl font-bold uppercase mb-6 text-gray-800 tracking-tight">
+            Maco<span className="text-blue-600">RMS</span>
+          </h2>
+          <p className="text-2xl md:text-3xl text-gray-600 mb-12">
+            Building <span className="text-blue-500 font-semibold">Relationships</span>
+          </p>
+         
         </div>
       </div>
     </>
